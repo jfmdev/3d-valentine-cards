78748e7298622c1028ab4b2b0bc8cce42d75b26e
Jose <jfmdev@gmail.com>
2025-05-13 20:47:18 -0300

    Fix Vite config missing Hears Pair entry

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        pair: resolve(__dirname, 'heartsPair.html'),
         rain: resolve(__dirname, 'heartsRain.html'),
         rotating: resolve(__dirname, 'rotatingHeart.html')
       }
